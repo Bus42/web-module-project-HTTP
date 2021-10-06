7747fa6d0cf311991a7249cbc7226e4f6a812a57
Greg <gbrewton1980@gmail.com>
2021-10-05 20:45:06 -0600

    Meets MVP

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import EditMovieForm from "./components/EditMovieForm";
 import FavoriteMovieList from "./components/FavoriteMovieList";
 
 import axios from "axios";
+import AddMovieForm from "./components/AddMovieForm";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
@@ -50,6 +51,10 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies} />
 
           <Switch>
+            <Route path="/movies/addmovie" >
+              <AddMovieForm setMovies={setMovies} />
+            </Route>
+
             <Route path="/movies/edit/:id">
               <EditMovieForm setMovies={setMovies} />
             </Route>
